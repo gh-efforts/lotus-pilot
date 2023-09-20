f7ee0851b40fe999496f4f9b85c8a975fb4bc3f9
Jack <yh.io@outlook.com>
2023-09-20 12:42:50 +0800

    fix worker tmpl

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,3 +9,13 @@ tmp_dir=$base_dir/tmp
 parent_cache_dir=$base_dir/parent_cache
 
 mkdir -p $repo_dir $log_dir $tmp_dir $parent_cache_dir
+
+while
+  port=$(shuf -n 1 -i 49152-65535)
+  netstat -atun | grep -q "$port"
+do
+  continue
+done
+listen=0.0.0.0:$port
+
+echo $listen
